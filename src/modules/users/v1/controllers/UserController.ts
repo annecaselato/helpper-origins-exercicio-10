f2c33bdd4cef4e628a9ab3ca3f9ac15dba542330
annecaselato <annecaselato@gmail.com>
2021-11-18 20:50:43 -0300

    Revert "Update UserController.ts"
    This reverts commit 4d2be50d3d6f14ebdd5a4702619ee5eba8aec014.

--- src/modules/users/v1/controllers/UserController.ts
+++ src/modules/users/v1/controllers/UserController.ts
@@ -94,22 +94,10 @@ export class UserController extends BaseController {
      *             type: object
      *             example:
      *               name: userName
-     *               email: userEmail
-     *               phone: userPhone
-     *               status: Ativo
      *             required:
      *               - name
-     *               - email
-     *               - phone
-     *               - status
      *             properties:
      *               name:
-     *                 type: string
-     *               email:
-     *                 type: string
-     *               phone:
-     *                 type: string
-     *               status:
      *                 type: string
      *     responses:
      *       $ref: '#/components/responses/baseCreate'
@@ -119,10 +107,7 @@ export class UserController extends BaseController {
     @Middlewares(UserValidator.post())
     public async add(req: Request, res: Response): Promise<void> {
         const newUser: DeepPartial<User> = {
-            name: req.body.name,
-            email: req.body.email,
-            phone: req.body.phone,
-            status: req.body.status
+            name: req.body.name
         };
 
         await new UserRepository().insert(newUser);
