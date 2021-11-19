2b472a202dad62cf0ea29b520950c40fec767cdb
annecaselato <annecaselato@gmail.com>
2021-11-19 09:07:09 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,11 @@ import { dbConfig } from './config/database';
 import { swaggerConfig } from './config/swagger';
 
 // Endpoints
-import { ClientController } from './modules/users/v1';
+import { UserController, ClientController } from './modules/users/v1';
 
 const app: App = new App({
     port: Number(process.env.PORT || 8080),
-    controllers: [ClientController],
+    controllers: [UserController, ClientController],
     middlewares: [Logger.middleware],
     logger: new Logger(),
     swaggerOptions: process.env.NODE_ENV === 'development' ? swaggerConfig : undefined,
